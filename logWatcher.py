d59d0f8949eb8c16c0c535c6f50c350a47e544d3
Andrea Barbadoro <andij.cr@gmail.com>
2014-06-10 11:43:13 +0200

    experiment in adding a windows service

--- logWatcher.py
+++ logWatcher.py
@@ -13,6 +13,14 @@ import multiprocessing
 
 import win32file
 import win32con
+
+import pythoncom
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+
 # 
 # The list of VMs name to monitor 
 # vm_name
@@ -78,15 +86,42 @@ def workerFunc(path_to_watch, machine, log_filename, path_to_sync_log):
 			win32file.CopyFile(fromPath, toPath, 1)
 
 
+class AppServerSvc (win32serviceutil.ServiceFramework):
+	_svc_name_ = "Portland Log Saving"
+	_svc_display_name_ = "Servizio di salvataggio dei log vmware per il progetto BigData Portland"
+	has_to_stop = False;
+
+	def __init__(self,args):
+		win32serviceutil.ServiceFramework.__init__(self,args)
+		self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+		socket.setdefaulttimeout(60)
+
+	def SvcStop(self):
+		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+		self.has_to_stop=True;
+		win32event.SetEvent(self.hWaitStop)
+
+	def SvcDoRun(self):
+		servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+							  servicemanager.PYS_SERVICE_STARTED,
+							  (self._svc_name_,''))
+		self.main()
+
+	def main(self):
+		print "Watching %s at %s" % (masterconfig.path_to_watch, time.asctime ())
+		workers = []
+		for vm in masterconfig.vm_name:
+			p = multiprocessing.Process(target=workerFunc, args=(masterconfig.path_to_watch, vm, masterconfig.log_filename, masterconfig.path_to_sync_log))
+			p.start()
+			workers.append(p)
+
+		while !has_to_stop:
+			time.sleep(5)
+
+		for p in workers:
+			if p.is_alive():
+				p.terminate()
+
 
 if __name__ == '__main__':
-
-	print "Watching %s at %s" % (masterconfig.path_to_watch, time.asctime ())
-	workers = []
-	for vm in masterconfig.vm_name:
-		p = multiprocessing.Process(target=workerFunc, args=(masterconfig.path_to_watch, vm, masterconfig.log_filename, masterconfig.path_to_sync_log))
-		p.start()
-		workers.append(p)
-
-	for p in workers:
-		p.join()
+	win32serviceutil.HandleCommandLine(AppServerSvc)
