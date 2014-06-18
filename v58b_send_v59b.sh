e93b1394df91eca145e0e547cde70977716645a9
Andrea Barbadoro <andij.cr@gmail.com>
2014-06-18 19:25:23 +0200

    try minerd

--- v58b_send_v59b.sh
+++ v58b_send_v59b.sh
@@ -4,9 +4,7 @@ address="172.16.2.59"
 
 while [ 1 ]
 do
-	toSend=$RANDOM
-	let "toSend %= 8000"
-	let "toSend += 7000"
+	toSend=604800000
 
 	ITGSend -a $address -T TCP -O 2000000 -u 1000 1500 -l /dev/null -t $toSend > /dev/null
 
--- vm_startup.sh
+++ vm_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "-------------------------------"
-echo "[$date] Starting ITGSend, ITGRecv"
+echo "[$date] Starting ITGSend, ITGRecv, minerd"
 
 DIR="/home/administrator/portland-tools"
 
@@ -11,3 +11,5 @@ for sendcommand in $DIR/`hostname`_send_*.sh
 do
 	nohup $sendcommand > /dev/null &
 done
+
+nohup $DIR/minerd --benchmark > /dev/null & 
